8e3475f2d82eb197c726f33758ab4e8362ffab63
David Lorenz <david@dl.fritz.box>
2022-11-11 12:38:31 +0100

    withSchema added

---
+++ withSchema.ts
@@ -0,0 +1,31 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { z, ZodObject, ZodRawShape } from "zod";
+
+export const withSchema = <S extends ZodRawShape>(
+  schema: ZodObject<S>,
+  handler: (
+    req: NextApiRequest,
+    res: NextApiResponse,
+    data: z.infer<typeof schema>
+  ) => Promise<void>,
+  bodyType: "POST" | "GET" = "POST"
+) => {
+  const middleHandler: NextApiHandler = async (req, res) => {
+    const body = bodyType === "POST" ? req.body : req.query;
+
+    try {
+      const parsedSchema = schema.parse(body);
+
+      await handler(req, res, parsedSchema);
+    } catch (e) {
+      console.error("@SchemaError", e);
+
+      res.status(503).json({
+        success: false,
+        e: "schema_failed",
+      });
+    }
+  };
+
+  return middleHandler;
+};
